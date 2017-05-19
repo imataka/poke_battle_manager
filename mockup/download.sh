573421556cb330d02dfde0cffbd74801bd0ec170
Ryo Yoshitani <ry.3607x5531@gmail.com>
2017-05-19 16:02:42 +0900

    add mockup

---
+++ mockup/download.sh
@@ -0,0 +1,21 @@
+#for i in `seq 1 801`; do
+#    wget http://img.yakkun.com/poke/sm/n$i.gif -O fig/n`printf %03i $i`.gif
+#done
+
+#TESTFILE=./alora.txt
+#while read line; do
+#    wget http://img.yakkun.com/poke/sm/n${line}a.gif -O fig/n`printf %03i ${line}`a.gif
+#done < $TESTFILE
+#
+#TESTFILE=./mega.txt
+#while read line; do
+#    wget http://img.yakkun.com/poke/sm/n${line}m.gif -O fig/n`printf %03i ${line}`m.gif
+#done < $TESTFILE
+
+#wget http://img.yakkun.com/poke/sm/n382p.gif -P fig
+#wget http://img.yakkun.com/poke/sm/n383p.gif -P fig
+
+wget http://img.yakkun.com/poke/sm/n6x.gif -P fig
+wget http://img.yakkun.com/poke/sm/n6y.gif -P fig
+wget http://img.yakkun.com/poke/sm/n150x.gif -P fig
+wget http://img.yakkun.com/poke/sm/n150y.gif -P fig
