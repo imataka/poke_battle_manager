716e2045354b89916efea912ac0970a4d01cf8f9
Ryo Yoshitani <ry.3607x5531@gmail.com>
2017-05-22 15:59:25 +0900

    generate controllers battles parties stats

---
+++ app/controllers/battles_controller.rb
@@ -0,0 +1,13 @@
+class BattlesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  get 'battles/index'
+
+  get 'battles/new'
+
+  get 'battles/show'
+
+  get 'battles/edit'
+
+  get 'parties/index'
+
+  get 'parties/new'
+
+  get 'parties/show'
+
+  get 'parties/edit'
+
+  get 'stats/index'
+
+  get 'stats/new'
+
+  get 'stats/show'
+
+  get 'stats/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
