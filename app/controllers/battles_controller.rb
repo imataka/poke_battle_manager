fc75f194a1f6ea9a5be0719adf90a61aa53fa31e
imataka <imataka00000@gmail.com>
2017-06-18 21:22:26 +0900

    no message

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -3,11 +3,20 @@ class BattlesController < ApplicationController
   end
 
   def new
+    @battle = Battle.new
+    @battle.opp_pokes.build
+    @battle.my_pokes.build
   end
 
   def show
   end
 
-  def edit
+  def create
+    Battle.create(battle_params)
+  end
+
+private
+  def battle_params
+    params.require(:battle).permit(:my_rate, :opp_rate, opp_pokes_attributes: [:name, :item], my_pokes_attributes: [:name, :item])
   end
 end
