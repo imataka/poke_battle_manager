efb3ef9da70bdcee172084d3dd5fc59e308de0ca
Ryo Yoshitani <ry.3607x5531@gmail.com>
2017-06-18 23:23:03 +0900

    revise associations of models

--- app/models/my_poke.rb
+++ app/models/my_poke.rb
@@ -1,5 +1,5 @@
 class MyPoke < ActiveRecord::Base
   has_many :battle_my_pokes, dependent: :destroy
-  has_many :battles, :through => :battle_my_poke
-  has_many :eval, dependent: :destroy
+  has_many :battles, :through => :battle_my_pokes
+  has_many :evals, dependent: :destroy
 end
--- app/models/opp_poke.rb
+++ app/models/opp_poke.rb
@@ -1,5 +1,5 @@
 class OppPoke < ActiveRecord::Base
   has_many :battle_opp_pokes, dependent: :destroy
-  has_many :battles, :through => :battle_opp_poke
-  has_many :eval, dependent: :destroy
+  has_many :battles, :through => :battle_opp_pokes
+  has_many :evals, dependent: :destroy
 end
