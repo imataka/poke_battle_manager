c7edc8fb31f7ae4204291f6422047745701342c9
Ryo Yoshitani <ry.3607x5531@gmail.com>
2017-06-24 23:27:30 +0900

    finished, maybe...

--- app/controllers/evals_controller.rb
+++ app/controllers/evals_controller.rb
@@ -6,17 +6,16 @@ class EvalsController < ApplicationController
     @battle = Battle.find(session[:battle_id])
     @choices = (-3..3).to_a.reverse.map {|i| [i, i] } # [[3, 3], [2, 2], ..., [-3, -3]]
     (@battle.my_pokes.count * @battle.opp_pokes.count).times do
-      @battle.evals.build
+      @battle.evals.build.save
     end
   end
 
-  def update
-    battle = Battle.find(battle_params[:id])
-    battle_params[:evals_attributes].each do |e|
-      evaluation = Eval.new(eval_params)
-      battle.evals << evaluation
+  def create
+    evals_params.map do |id, eval_param|
+      e = Eval.find(id)
+      e.update_attributes(eval_param)
+      e.save
     end
-    battle.save
     redirect_to controller: :battles, action: :index
   end
 
@@ -27,7 +26,7 @@ class EvalsController < ApplicationController
   end
 
   private
-  def battle_params
-    params.require(:battle).permit(:id, evals_attributes: [:eval, :my_poke_id, :opp_poke_id])
+  def evals_params
+    params.permit(evals: [:eval, :my_poke_id, :opp_poke_id])[:evals]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-root 'battles#index'
+  root 'battles#index'
   resources :battles
-  resources :evals
+  resource :evals
 
 
   # The priority is based upon order of creation: first created -> highest priority.
