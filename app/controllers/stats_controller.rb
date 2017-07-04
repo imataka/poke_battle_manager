d751e6ccab417bf4b7181b4af5c0a46cda37eb0e
Ryo Yoshitani <ry.3607x5531@gmail.com>
2017-07-04 19:48:45 +0900

    implement controller/view

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,6 +6,10 @@ class StatsController < ApplicationController
   end
 
   def show
+    # TODO: 現在パーティにいるポケモンと全てのポケモンでtoggle
+    @my_pokes = MyPoke.all
+    @opp_names = OppPoke.sorted_name
+    @opp_ids = @opp_names.map {|name| OppPoke.ids_by_name(name) }
   end
 
   def edit
