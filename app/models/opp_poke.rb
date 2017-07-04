8c8c7628cd97603e605caef9e83d8a7f0201a713
Ryo Yoshitani <ry.3607x5531@gmail.com>
2017-07-04 18:48:27 +0900

    implement class method `ids_by_name`

--- app/models/opp_poke.rb
+++ app/models/opp_poke.rb
@@ -4,4 +4,11 @@ class OppPoke < ActiveRecord::Base
   has_many :evals, dependent: :destroy
 
   accepts_nested_attributes_for :battle_opp_pokes, allow_destroy: true
+
+  # Returns Array of id of objects whose name corresponds to argument
+  class << self
+    def ids_by_name(name)
+      self.where(name: name).map { |o| o.id }
+    end
+  end
 end
