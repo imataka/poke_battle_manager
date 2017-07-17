902229f95d0680113924fa89306c39e986d00334
Ryo Yoshitani <ry.3607x5531@gmail.com>
2017-07-17 18:25:32 +0900

    add boolean is_party to MyPoke

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620115755) do
+ActiveRecord::Schema.define(version: 20170717092425) do
 
   create_table "battle_my_pokes", force: :cascade do |t|
     t.integer  "battle_id",  limit: 4
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20170620115755) do
     t.string   "ability",    limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.boolean  "in_party"
   end
 
   create_table "opp_pokes", force: :cascade do |t|
