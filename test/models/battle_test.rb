5eaed5df98974e69bea2b6dca8fee7fc3bfa3171
Ryo Yoshitani <ry.3607x5531@gmail.com>
2017-07-19 00:00:23 +0900

    add tests

--- test/models/battle_test.rb
+++ test/models/battle_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class BattleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @battle = Battle.new(my_rate: 1500, opp_rate: 1500, result: true)
+    3.times do |i|
+      @battle.my_pokes.build(id: i, name: PokeData.find(i).name)
+      @battle.opp_pokes.build(id: i, name: PokeData.find(i).name)
+    end
+  end
+
+  test "should be valid" do
+    assert @battle.valid?
+  end
+
+  test "my_rate should be present" do
+    @battle.my_rate = nil
+    assert_not @battle.valid?
+  end
+
+  test "opp_rate should be present" do
+    @battle.opp_rate = nil
+    assert_not @battle.valid?
+  end
+
+  test "battle.opp_poke should be unique" do
+    @battle.opp_pokes[0].name = @battle.opp_pokes[1].name
+    assert_not @battle.valid?
+  end
+
+  test "battle.battle_my_pokes.my_poke_id should be unique" do
+    @battle.my_pokes[0] = @battle.battle_my_pokes[1]
+    assert_not @battle.valid?
+  end
 end
--- test/models/my_poke_test.rb
+++ test/models/my_poke_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class MyPokeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @my_poke = MyPoke.new(name: PokeData.find(0).name, item: "きあいのタスキ", ability: "しんりょく")
+  end
+
+  test "should be valid" do
+    assert @my_poke.valid?
+  end
+
+  # 名前がありえるかチェック
+  test "my_poke.name should be poke name" do
+    @my_poke.name = "a"
+    assert_not @my_poke.valid?
+  end
+
+  # とくせいがありえるかチェック
 end
