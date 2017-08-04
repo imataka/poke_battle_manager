4096742413204637ecabbd05f7184ddd6d854f70
imataka <imataka00000@gmail.com>
2017-08-04 20:01:37 +0900

    Userテーブルにusernameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804104329) do
+ActiveRecord::Schema.define(version: 20170804105930) do
 
   create_table "battle_my_pokes", force: :cascade do |t|
     t.integer  "battle_id",  limit: 4
